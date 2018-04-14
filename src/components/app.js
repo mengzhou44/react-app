e9f0e15b0af943108b1111cbe88a462274bf59db
meng  zhou <mengzhou44@mengs-MacBook-Pro.local>
2018-04-14 15:47:13 -0600

    no comments

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Header from './_common/header';
 import Features from './home/features';
 import Meals from './home/meals';
+import Steps from './home/steps';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Header />
         <Features />
         <Meals />
+        <Steps />
       </div>
     );
   }
