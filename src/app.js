1c65dea6d3891fb123365d2b4b46e1a849657ff2
meng  zhou <mengzhou44@mengs-MacBook-Pro.local>
2018-04-13 21:02:17 -0600

    init commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <h1 className="title">Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
